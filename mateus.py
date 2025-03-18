555630217f69e48d765f69e67532bdfc78fcacc1
Mateus Silva <mateus.ferreira23@etepd.com>
2025-03-18 09:09:33 -0300

    Create mateus.py

---
+++ mateus.py
@@ -0,0 +1,6 @@
+nome = input("digite seu nome: ") # o input serve para o ususário colocar informações (variável)
+idade = input("digite sua idade:")
+sonho = input("o que você pretende fazer no futuro:")
+
+
+
